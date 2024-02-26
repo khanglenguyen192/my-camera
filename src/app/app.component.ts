7fdd26b26d1033b492cdcc1a4feb999a4687fe7c
Khang Le <khang.lenguyen192@gmail.com>
2024-02-26 13:50:13 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-camera';
+  private trigger: Subject<any> = new Subject();
+
+  public webcamImage!: WebcamImage;
+  private nextWebcam: Subject<any> = new Subject();
+
+  captureImage  = '';
+  
+  ngOnInit() {}
+
+  /*------------------------------------------
+    --------------------------------------------
+    triggerSnapshot()
+    --------------------------------------------
+    --------------------------------------------*/
+    public triggerSnapshot(): void {
+      this.trigger.next("");
+  }
+
+  /*------------------------------------------
+  --------------------------------------------
+  handleImage()
+  --------------------------------------------
+  --------------------------------------------*/
+  public handleImage(webcamImage: WebcamImage): void {
+      this.webcamImage = webcamImage;
+      this.captureImage = webcamImage!.imageAsDataUrl;
+      console.info('received webcam image', this.captureImage);
+  }
+
+  /*------------------------------------------
+  --------------------------------------------
+  triggerObservable()
+  --------------------------------------------
+  --------------------------------------------*/
+  public get triggerObservable(): Observable<any> {
+
+      return this.trigger.asObservable();
+  }
+
+  /*------------------------------------------
+  --------------------------------------------
+  nextWebcamObservable()
+  --------------------------------------------
+  --------------------------------------------*/
+  public get nextWebcamObservable(): Observable<any> {
+
+      return this.nextWebcam.asObservable();
+  }
 }
